5c3c813739df097d05fe3681956aae402bdf50b1
Aadityayadav786 <aady10748@gmail.com>
2025-05-22 11:02:13 +0530

    Fix GitHub Actions: Add PYTHONPATH for pytest

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,11 +1,7 @@
-from src.math_operation import add,sub
+# src/math_operation.py
 
-def test_add():
-    assert add(2,3) == 5
-    assert add(-1,1) == 0
-    
-def test_sub():
-    assert sub(5,3)==2
-    assert sub(4,3)==1
-    assert sub(3,3)==0
-    assert sub(2,3)==-1
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
